3e203a4915a71fbcc03e5cc79830fe93e980391d
Roman Vashurin <rvashurin@gmail.com>
2025-12-30 14:50:02 +0300

    Add to simple estimators

--- src/lm_polygraph/utils/factory_estimator.py
+++ src/lm_polygraph/utils/factory_estimator.py
@@ -58,7 +58,12 @@ def load_simple_estimators(name: str, config):
         RAUQ,
         CSL,
         SemanticDensity,
-        SpecificationUncertainty,
+        SpecificationUncertaintySemantic
+        SpecificationUncertaintySemanticNormalized
+        SpecificationUncertaintySemanticDirect
+        SpecificationUncertaintySemanticDirectNormalized
+        SpecificationUncertaintyMCSE
+        SpecificationUncertaintyMCNSE
     ]
 
     try:
